196a5ac9f87ff267ad2e58fda63e47ba33627060
Aniruddha Velankar <aniruddha@azafashions.com>
2024-11-28 22:37:47 +0530

    Added handling for the single number & two comma seperated numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -8,4 +8,8 @@ describe("String Calculator", () => {
   it("returns the number itself for a single number", () => {
     expect(add("1")).toBe(1);
   });
+
+  it("returns the sum of two numbers", () => {
+    expect(add("1,2")).toBe(3);
+  });
 });
