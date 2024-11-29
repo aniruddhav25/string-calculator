f112c3882508868b047db75c99621b233ab75cae
Aniruddha Velankar <aniruddha@azafashions.com>
2024-11-29 09:38:47 +0530

    Refactored the code to handle new test of supporting custom delimeter

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,6 +1,16 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
+
+  let delimiter = /,|\n/;
   
-  const numArray = numbers.split(/,|\n/).map(Number);
+  if (numbers.startsWith("//")) {
+    const match = numbers.match(/^\/\/(.+)\n/);
+    if (match) {
+      delimiter = new RegExp(match[1]);
+      numbers = numbers.slice(match[0].length);
+    }
+  }
+
+  const numArray = numbers.split(delimiter).map(Number);
   return numArray.reduce((sum, num) => sum + num, 0);
 }
